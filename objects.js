bec20e8ab236b43a85879888745eb0a9180c84a5
Ricky Rojas <rickyrojas19@gmail.com>
2018-10-22 19:12:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {
- 'Steep Leans': 'Trap House',
- 'Kendrick Lamar': 'Alright'
+ 'Steep Leans': 'Trap House'
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  Object.assign({}, playlist, {'Kendrick Lamar': 'Alright'});
+  return playlist;
 }
